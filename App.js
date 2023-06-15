f78c8e4593b5d1e382e550a8e73085eb5c788a97
Jagadeesh v <vemulajagadeesh28@gmail.com>
2023-06-15 10:48:28 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,19 +1,107 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "hello react1"),
-    React.createElement("h1", { id: "heading2" }, "hello react2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { id: "heading" }, "hello react"),
-    React.createElement("h1", { id: "heading2" }, "hello react2"),
-  ]),
-]);
+/* React.createElement => React element - js object => HTMLElement(Render) */
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Namasthe React Course"
+// );
 
-const heading = React.createElement("h1", { id: "heading" }, "hello world");
-// it will print the object not html element
-console.log(heading);
+/* Jsx (transpiles jsx before it reaches to Js) - Parcel - babel */
+/* Jsx - babel transpiles it to React.createElement => React Element - Js Object => HTMLElement(Render) */
+
+/* React Element */
+
+const heading = (
+  <h1 id="heading" tabIndex="5">
+    Namasthe React Using Jsx
+  </h1>
+);
+
+/* React Functional Component 2 ways*/
+
+// const HeadingComponent = () => {
+//   return <h1>Namasthe React Using Functional Component</h1>;
+// };
+
+// const HeadingComponent = () => <h1>Namasthe React Functional Component</h1>;
+
+/* Writting Function Based Components Using Function Keyword */
+
+// const Title = function () {
+//   return (
+//     <h1 id="heading" tabIndex="5">
+//       Namasthe React using Jsx
+//     </h1>
+//   );
+// };
+
+/* Functional Component Creation */
+
+const Title = () => (
+  <h1 id="heading" tabIndex="5">
+    Namasthe React Using Jsx
+  </h1>
+);
+
+/* Component Composition (writing component inside component) */
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 id="heading">Namasthe React Functional Component</h1>
+  </div>
+);
+
+/* multiple ways you can call Functional Component */
+/* no need to worry about malicious attack Jsx will take care of that */
+
+const data = 1000;
+
+const HeadingComponent2 = () => (
+  <div id="container">
+    {data}
+    {100 + 300}
+    {Title()}
+    <Title></Title>
+    <Title />
+    <h1 id="heading">Namasthe React Functional Component</h1>
+  </div>
+);
+
+/* Jsx expression should have one single root */
+
+const HeadingComponent3 = () => (
+  <div>
+    <div id="container"></div>
+    <div id="container2"></div>
+  </div>
+);
+
+/* React Fragment */
+
+const HeadingComponent4 = () => (
+  <React.Fragment>
+    <div id="container"></div>
+    <div id="container2"></div>
+  </React.Fragment>
+);
+
+/* React Fragment using shorthand notation */
+
+const HeadingComponent5 = () => (
+  <>
+    <div id="container"></div>
+    <div id="container2"></div>
+  </>
+);
+
+/* Creating Root using ReactDOM.createRoot */
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+/* rendering React Element */
+// root.render(heading);
+
+/* rendering React Functional Component */
+root.render(<HeadingComponent2 />);
--- README.md
+++ README.md
@@ -93,3 +93,65 @@ https://legacy.reactjs.org
 
 offical documentation of parcel
 https://parceljs.org/
+
+episode-03
+
+- add scripts in package.json so we can run application using npm run install or npm install
+
+- "scripts": {
+  "start": "parcel index.html",
+  "build": "parcel build index.html",
+  },
+
+- for build npm run build
+
+# JSX Information
+
+- jsx is javascript syntax which is easier to create the element and jsx is not part of React here jsx is diffrent and react is different
+
+- jsx is not a html or xml in JS u can say its html like syntax or xml like syntax
+
+- we can large application without using JSX as well but JSX makes our developers life easier
+
+- jsx is a convention where we merge all html and JS together
+
+# how JSX code works?
+
+- it transpiles JSX before it reaches to Js here transpilation will do the parcel
+
+- parcel will tell babel to transpiles(converting) the jsx code
+
+- finally JSX code will convert into React.createElement and that will convert into React element object then it will render as a html element using render method
+
+- you can see how jsx converted to react element in babel offical website https://babeljs.io/
+
+# why JSX is not html?
+
+- In JSX we write attribute className="something" but in html we write class="header" so here we write className and finally babel converts it to class
+
+# why we use parthensis for writting jsx code?
+
+- babel should know the start and end point so using the brackets we said to babel start and endpoint
+
+- for single line code brackets are not mandatory even you can keep it but for multiline code brackets or mandatory
+
+# React component ?
+
+- in React everything is one component
+
+2 types components
+
+1. Class Based Component (older)
+2. Function Based Component (latest)
+
+Ex: button is one component & header component & footer etc..
+
+# Functional Component ?
+
+-Functional component is a normal javascript function which return some piece of jsx code or React element and React function should start with capitial letter
+
+- we can write functions using function keyword as well but arrow function is newer way and industry standard everywhere you see array functions only most
+
+- jsx syntax take care malicious attack (xcross scripting site attack)
+
+- jsx expression must have one single parent
