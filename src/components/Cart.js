1120f16ef09f067440dc7dc1a25f68d4cd960fa7
Jagadeesh v <vemulajagadeesh28@gmail.com>
2023-06-29 20:27:31 +0530

    cartjs changes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,7 +4,13 @@ import { useDispatch } from "react-redux";
 import { clearItem } from "../utils/createSlice";
 
 const Cart = () => {
+  /* if u write like below line its like you are subscried to entire store so u dont need to write like this 
+   because each time it will be subscribed to entire store instead of slice of store because of that 
+   whenever store update each time it will rerender component  in large scale applications there will be lot of slices
+   so everytime u should subscribe to specific slice data only*/
+  //   const cartItems = useSelector((store) => store)
   const cartItems = useSelector((store) => store.cart.items);
+
   const dispatch = useDispatch();
   const clearItems = () => {
     dispatch(clearItem());
